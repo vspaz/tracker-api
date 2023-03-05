c81a848afed82a60327855369f279502e644a9b3
vspaz <vladimir.edelman@gmail.com>
2023-03-05 11:13:14 +0100

    minor fixes

--- src/api/health.rs
+++ src/api/health.rs
@@ -1,6 +1,6 @@
 use actix_web::{HttpRequest, HttpResponse, Responder};
 
 #[warn(unused_variables)]
-pub(crate) async fn ping(_req: HttpRequest) -> impl Responder {
+pub async fn ping(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().finish()
 }
--- src/api/server.rs
+++ src/api/server.rs
@@ -1,9 +1,13 @@
 use crate::api::handlers::register_handlers;
 use actix_web::HttpServer;
 
-pub async fn run() -> std::io::Result<()> {
+pub async fn start_server() -> std::io::Result<()> {
     HttpServer::new(|| register_handlers())
         .bind("127.0.0.1:8000")?
         .run()
         .await
+}
+
+pub async fn run() -> std::io::Result<()> {
+    start_server().await
 }
