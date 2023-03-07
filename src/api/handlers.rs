45d5efe30d09b8fc5ebdc65c56d2475082e796de
vspaz <vladimir.edelman@gmail.com>
2023-03-07 08:15:16 +0100

    add /identify & /i endpoints

--- src/api/handlers.rs
+++ src/api/handlers.rs
@@ -1,10 +1,9 @@
 use crate::api::health::ping;
-use crate::api::segment::{page, track};
+use crate::api::segment::{identify, page, track};
 use actix_web::body::MessageBody;
 use actix_web::dev::{ServiceFactory, ServiceRequest, ServiceResponse};
 use actix_web::web::{get, post};
 use actix_web::{App, Error};
-
 
 fn add_api_prefix_endpoint(endpoint: &str) -> String {
     "/api/v1/".to_owned() + endpoint
@@ -24,6 +23,8 @@ pub fn register_handlers() -> App<
         .route(&*add_api_prefix_endpoint("t"), post().to(track))
         .route(&*add_api_prefix_endpoint("page"), post().to(page))
         .route(&*add_api_prefix_endpoint("p"), post().to(page))
+        .route(&*add_api_prefix_endpoint("identify"), post().to(identify))
+        .route(&*add_api_prefix_endpoint("i"), post().to(identify))
         // service endpoint
         .route("/ping/", get().to(ping))
         .route("/", get().to(ping))
--- src/api/segment.rs
+++ src/api/segment.rs
@@ -9,3 +9,8 @@ pub async fn track(_req: HttpRequest) -> impl Responder {
 pub async fn page(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().finish()
 }
+
+#[warn(unused_variables)]
+pub async fn identify(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
