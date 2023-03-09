e3558b8932af19c3a8a1be66d2287b8647b0099c
vspaz <vladimir.edelman@gmail.com>
2023-03-09 18:47:58 +0100

    minor style fix

--- src/api/health.rs
+++ src/api/health.rs
@@ -1,8 +1,8 @@
+use actix_web::{web, HttpRequest, HttpResponse, Responder};
 use std::collections::HashMap;
-use actix_web::{HttpRequest, HttpResponse, Responder, web};
 
 #[warn(unused_variables)]
 pub async fn ping(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().finish();
-    return web::Json(HashMap::from([("ping", "pong")]))
+    return web::Json(HashMap::from([("ping", "pong")]));
 }
