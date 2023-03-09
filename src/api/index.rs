8a6fec3621d3f58a47b7a4827eb2746a45c1742e
vspaz <vladimir.edelman@gmail.com>
2023-03-09 18:30:36 +0100

    extend other structs

--- src/api/index.rs
+++ src/api/index.rs
@@ -1,5 +1,5 @@
-use actix_web::{web, HttpRequest, HttpResponse, Responder};
 use crate::api::handlers::ResponseOk;
+use actix_web::{web, HttpRequest, HttpResponse, Responder};
 
 #[warn(unused_variables)]
 pub async fn index(_req: HttpRequest) -> impl Responder {
--- src/api/segment.rs
+++ src/api/segment.rs
@@ -1,4 +1,5 @@
-use actix_web::{HttpRequest, HttpResponse, Responder};
+use crate::api::handlers::ResponseOk;
+use actix_web::{web, HttpRequest, HttpResponse, Responder};
 
 #[warn(unused_variables)]
 pub async fn track(_req: HttpRequest) -> impl Responder {
@@ -7,30 +8,54 @@ pub async fn track(_req: HttpRequest) -> impl Responder {
 
 #[warn(unused_variables)]
 pub async fn page(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
+    HttpResponse::Ok().finish();
+    return web::Json(ResponseOk {
+        status: "200 OK".to_string(),
+        message: "OK".to_string(),
+    });
 }
 
 #[warn(unused_variables)]
 pub async fn identify(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
+    HttpResponse::Ok().finish();
+    return web::Json(ResponseOk {
+        status: "200 OK".to_string(),
+        message: "OK".to_string(),
+    });
 }
 
 #[warn(unused_variables)]
 pub async fn alias(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
+    HttpResponse::Ok().finish();
+    return web::Json(ResponseOk {
+        status: "200 OK".to_string(),
+        message: "OK".to_string(),
+    });
 }
 
 #[warn(unused_variables)]
 pub async fn screen(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
+    HttpResponse::Ok().finish();
+    return web::Json(ResponseOk {
+        status: "200 OK".to_string(),
+        message: "OK".to_string(),
+    });
 }
 
 #[warn(unused_variables)]
 pub async fn batch(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
+    HttpResponse::Ok().finish();
+    return web::Json(ResponseOk {
+        status: "200 OK".to_string(),
+        message: "OK".to_string(),
+    });
 }
 
 #[warn(unused_variables)]
 pub async fn import(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
+    HttpResponse::Ok().finish();
+    return web::Json(ResponseOk {
+        status: "200 OK".to_string(),
+        message: "OK".to_string(),
+    });
 }
