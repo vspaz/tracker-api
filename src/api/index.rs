58de5a7b48604c54ea07f5cfa19d49aa3a444f50
vspaz <vladimir.edelman@gmail.com>
2023-03-09 18:26:05 +0100

    add json reponse

--- src/api/index.rs
+++ src/api/index.rs
@@ -1,6 +1,14 @@
-use actix_web::{HttpRequest, HttpResponse, Responder};
+use actix_web::{HttpRequest, HttpResponse, Responder, web};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct ResponseOk {
+    status: String,
+    message: String,
+}
 
 #[warn(unused_variables)]
 pub async fn index(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
+    HttpResponse::Ok().finish();
+    return web::Json(ResponseOk{ status: "200 OK".to_string(), message: "OK".to_string() })
 }
