3366a3bf6bb3c8469adaf83b4ebf9ae261b7c209
vspaz <vladimir.edelman@gmail.com>
2023-03-09 18:36:49 +0100

    extende ping method

--- src/api/health.rs
+++ src/api/health.rs
@@ -1,6 +1,8 @@
-use actix_web::{HttpRequest, HttpResponse, Responder};
+use std::collections::HashMap;
+use actix_web::{HttpRequest, HttpResponse, Responder, web};
 
 #[warn(unused_variables)]
 pub async fn ping(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
+    HttpResponse::Ok().finish();
+    return web::Json(HashMap::from([("ping", "pong")]))
 }
