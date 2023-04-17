7a44dc7dd3761c98c2aaedd499a4e0239b55d84c
vspaz <vladimir.edelman@gmail.com>
2023-04-17 22:36:21 +0200

    add get_config funciton

--- src/config.rs
+++ src/config.rs
@@ -2,3 +2,11 @@ pub struct Config {
     pub server: String,
     pub port: String,
 }
+
+
+pub fn get_config() -> Config {
+    Config {
+        server: "127.0.0.1".to_string(),
+        port: "9000".to_string(),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 mod api;
 mod config;
-use crate::config::Config;
+
 
 use crate::api::app;
 use actix_web::body::MessageBody;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let config = Config {
-        server: "127.0.0.1".to_string(),
-        port: "9000".to_string(),
-    };
-
+    let config = config::get_config();
     app::run(&config).await
 }
