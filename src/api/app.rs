0aba7dd5e52a713b4788f608df821d16f2c8d943
vspaz <vladimir.edelman@gmail.com>
2023-04-20 22:11:25 +0200

    fix duplicated message

--- src/api/app.rs
+++ src/api/app.rs
@@ -4,9 +4,10 @@ use actix_web::HttpServer;
 
 pub async fn start_server(config: &Config) -> std::io::Result<()> {
     let host_and_port = &format!("{}:{}", config.http.server.host, config.http.server.port);
+    let app = HttpServer::new(register_handlers);
+    info!("handlers configured");
     info!("starting server at '{}'", host_and_port);
-    HttpServer::new(register_handlers)
-        .keep_alive(config.http.server.keep_alive)
+    app.keep_alive(config.http.server.keep_alive)
         .bind(host_and_port)?
         .run()
         .await
